ffca06ea8ddad03a87b4a01a7a5ad390c9951e32
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-12 20:47:49 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const bodyParser = require('body-parser')
-const { check, validationResult} = require('express-validator')
-const bcrypt = require('bcryptjs')
 const app = express()
 const session = require('express-session')
 const Web3 = require('web3')
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
+const router = require('./router')
+
 config.dev = process.env.NODE_ENV !== 'production'
 
-const models = require('./models/index.js')
 
 async function start() {
 
@@ -37,50 +37,8 @@ async function start() {
   })
   )
 
-  app.post(
-    '/api/users', 
-    [
-      check('email')
-        .isEmail()
-        .normalizeEmail(), 
-        check('password').isLength({min: 6})
-      ],
-      async (req,res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      console.log(errors)
-      return res.status(422).json({ errors: errors.array(
-      )   })
-    }
-    console.log(req.body)
+  app.use('/', router)
 
-    try {
-      const salt = await bcrypt.genSalt(10)
-      console.log(salt)
-
-      const hash = await bcrypt.hash(req.body.password, salt)
-      models.User.create({
-        email: req.body.email, 
-        password: hash
-      })
-
-      req.session.user ={
-        isAuthenticated: true
-      }
-
-      res.status(200).json(req.session.user)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-)
-
-  app.post('/api/logout', (req,res) => {
-    req.session.destroy((err) => {
-      console.log('12345')
-      res.end()
-    })
-  })
  
   // Give nuxt middleware to express
   app.use(nuxt.render)
