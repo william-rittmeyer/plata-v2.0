d4a1a4973fd73c9b4679b741cd09986243c688d0
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-12 20:48:09 -0400

    Add files via upload

---
+++ server/controllers/User.js
@@ -0,0 +1,159 @@
+const crypto = require('crypto')
+const moment = require('moment')
+const { check, validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs')
+const mailgun = require('mailgun-js');
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, 
+	domain: process.env.MAILGUN_DOMAIN
+});
+const models = require('../models/index.js')
+
+exports.create = async (req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array(
+      )   })
+    }
+
+    try {
+      const salt = await bcrypt.genSalt(10)
+
+      const hash = await bcrypt.hash(req.body.password, salt)
+      let user = await models.User.create({
+        email: req.body.email, 
+        password: hash
+      })
+
+      user = user.toJSON()
+      user.isAuthenticated = true
+      delete user.password
+
+
+      req.session.user = user
+
+      res.status(200).json(req.session.user)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  exports.login = async (req,res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+      return res.status(422).json({ errors:
+       errors.array()   })
+      } else{
+
+      let user = await models.User.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+
+      if (!user) {
+        res.status(401).end('email does not exist')
+      }
+
+      const isValid = await bcrypt.compare(req.
+        body.password, user.password)
+
+      if (isValid == false ) {
+        res.status(401).end('username or password invalid')
+      }
+
+      user = user.toJSON()
+      user.isAuthenticated = true
+      delete user.password
+
+
+      req.session.user = user
+
+      res.json(req.session.user)
+    }
+    }
+
+    exports.sendResetPasswordRequest = async (req,res) => {
+    	try{
+    	const user = await models.User.findOne({
+    		where: {
+    			email: req.body.email
+    		}
+    	})
+
+    	if (!user) {
+    		return res.status(404).end('no user with this email exists')
+    	}
+
+
+    	const token = crypto.randomBytes(16).toString('hex')
+
+    	await user.update({
+    		resetPasswordToken: token,
+    		resetPasswordTokenExpiration: moment().
+    		add(30, 'minute').
+    		format()
+    	})
+
+    	const data = {
+			from: "Mailgun Sandbox <postmaster@sandbox9140bcda9e464aca823514e9c0dc3e28.mailgun.org>",
+			to: req.body.email,
+			subject: "Password Reset Request",
+			text: `http://${req.headers.host}/reset-password/${token}`
+};
+		const body = await mg.messages().send(data)
+		console.log(body)
+		console.log(req.body.email)
+
+
+    	res.json(token)
+    } catch (err) {
+    	throw new Error(err)
+    }
+}
+
+exports.getUserByResetPasswordToken =  async (req,res) => {
+  try{
+const user = await models.User.findOne({
+        where: {
+          resetPasswordToken: req.params.token
+        }
+      })
+
+      if (!user) {
+        return res.status(500).end('no user found')
+      }
+
+    res.json(user)
+    } catch (err) {
+      throw new Error(err)
+    }
+}
+
+exports.resetPassword = async (req, res) => {
+  try {
+
+  const user = await models.User.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+
+      if (!user) {
+        return res.status(404).end('user does not exist')
+      }
+
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(req.body.password, salt)
+
+
+      await user.update({
+        password: hash,
+        resetPasswordToken: token,
+        resetPasswordTokenExpiration: null
+      })
+      res.end()
+
+      } catch (err) {
+      throw new Error(err)
+    }
+}
---
+++ server/router.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { check } = require('express-validator')
+const UserController = require('./controllers/User.js')
+const router = express.Router()
+
+router.put('/api/users/:id/resetPassword', 
+  UserController.resetPassword)
+
+router.get('/api/users/tokens/:token', UserController.getUserByResetPasswordToken)
+
+router.post(
+    '/api/users', 
+    [
+      check('email')
+        .isEmail()
+        .normalizeEmail(), 
+        check('password').isLength({min: 6})
+      ], 
+      UserController.create
+)
+
+    router.post('/api/users/login', 
+      [
+      check('email')
+        .isEmail()
+        .normalizeEmail(), 
+        check('password').isLength({min: 6})
+      ], UserController.login
+    )
+
+    router.post('/api/users/sendResetPasswordRequest', 
+      UserController.sendResetPasswordRequest)
+
+
+
+  router.post('/api/logout',  (req,res) => {
+    req.session.destroy((err) => {
+      res.end()
+    })
+  })
+
+  module.exports = router
