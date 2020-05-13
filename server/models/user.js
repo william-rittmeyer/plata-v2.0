a4aaa3c1e6bae1998cc963c2e1832e8a0fa6989c
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-12 20:50:03 -0400

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define(
+  	'User', 
+  	{
     email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+    password: DataTypes.STRING,
+    resetPasswordToken: DataTypes.STRING,
+    resetPasswordTokenExpiration: DataTypes.
+    DATE
+  }, 
+  {}
+);
   User.associate = function(models) {
     // associations can be defined here
   };
