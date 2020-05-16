c519d327daf64a034b3443d148f701d8812bb45e
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-15 21:19:51 -0400

    Update _token.vue

--- pages/reset-password/_token.vue
+++ pages/reset-password/_token.vue
@@ -190,8 +190,8 @@ body {
   border-radius: 5px;
   box-shadow: 0 0 5px #03e9f4,
               0 0 25px #03e9f4,
-              0 0 50px #03e9f4,
-              0 0 100px #03e9f4;
+              0 0 0px #03e9f4,
+              0 0 0px #03e9f4;
 } 
 
 .login-box button:hover {
@@ -200,8 +200,8 @@ body {
   border-radius: 5px;
   box-shadow: 0 0 5px #03e9f4,
               0 0 25px #03e9f4,
-              0 0 50px #03e9f4,
-              0 0 100px #03e9f4;
+              0 0 0px #03e9f4,
+              0 0 0px #03e9f4;
 }
 
 .login-box a span {
