c7f37b33464128ccbc72ec6eaf5399ac59da739b
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-15 21:20:28 -0400

    Update index.vue

--- pages/reset-password/index.vue
+++ pages/reset-password/index.vue
@@ -159,8 +159,8 @@ body {
   border-radius: 5px;
   box-shadow: 0 0 5px #03e9f4,
               0 0 25px #03e9f4,
-              0 0 50px #03e9f4,
-              0 0 100px #03e9f4;
+              0 0 0px #03e9f4,
+              0 0 0px #03e9f4;
 } 
 
 .login-box button:hover {
@@ -169,8 +169,8 @@ body {
   border-radius: 5px;
   box-shadow: 0 0 5px #03e9f4,
               0 0 25px #03e9f4,
-              0 0 50px #03e9f4,
-              0 0 100px #03e9f4;
+              0 0 0px #03e9f4,
+              0 0 0px #03e9f4;
 }
 
 .login-box a span {
