7d77711ea5c1ebb0eed05bed06693d89114755a8
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-17 15:39:10 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,7 +38,7 @@
 
           <nuxt-link
             v-if="$store.state.user"
-            to="/sell"
+            to="/database"
             class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
           >
           Database Access
@@ -144,7 +144,9 @@ export default {
 
       this.$store.commit('SET_USER',null)
 
-      if (this.$route.path === '/profile') 
+      if ((this.$route.path === '/profile') ||
+      (this.$route.path === '/sell') ||
+      (this.$route.path === '/database') )
         { 
         this.$router.push('/')
 
