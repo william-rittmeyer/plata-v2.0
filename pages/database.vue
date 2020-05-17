d516b90a1c223517a071a19a3f507040106c6b92
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-17 15:41:18 -0400

    Add files via upload

---
+++ pages/database.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <section>
+   <h1 class="database">
+        Database access page
+      </h1>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+
+	fetch({store, redirect}) {
+	if (
+	!store.state.user || 
+	(store.state.user && store.state.user.
+	isAuthenticated ===false)
+	) {
+	return redirect('/')
+	}
+	}
+
+}
+
+</script>
+
+<style lang="css">
+
+.database{
+	position: absolute;
+	top:50%;
+	left: 35%;
+	font-family: sans-serif;
+  	display: block;
+  	font-weight: 300;
+  	font-size: 50px;
+  	color: white;
+  	letter-spacing: 1px;
+}
+
+
+</style>
