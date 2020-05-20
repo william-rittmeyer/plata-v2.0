ed8831027411aee49eea4ae716799b3a7d96a689
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-19 21:31:51 -0400

    Update account.vue

--- pages/account.vue
+++ pages/account.vue
@@ -1,26 +1,187 @@
 <template>
-  <section>
-
-    <h1>Account Page</h1>
-
-  </section>
+  <div class="table1">
+    <mdb-datatable-2 v-model="data" :searching="{value: search, field: 'office'}" />
+    <mdb-input class="mt-0" v-model="search" label="Search by office" />
+  </div>
 </template>
 
 <script>
-export default {
-	fetch({store, redirect}) {
-	if (
-	!store.state.user || 
-	(store.state.user && store.state.user.
-	isAuthenticated ===false)
-	) {
-	return redirect('/registration')
-	}
-	}
-}
+  import { mdbDatatable2, mdbInput } from 'mdbvue';
+  export default {
+    name: 'DatatablePage',
+    components: {
+      mdbDatatable2,
+      mdbInput
+    },
+    data () {
+      return {
+        search: '',
+        data: {
+          columns: [
+            {
+              label: 'Data',
+              field: 'data',
+              sort: true
+            },
+            {
+              label: 'Description',
+              field: 'description',
+              sort: true
+            },
+            {
+              label: 'Price',
+              field: 'price',
+              sort: false,
+              format: value => value + " ETH"
+            },
+            {
+              label: 'Seller',
+              field: 'seller',
+              sort: true
+            },
+          ],
+           rows: [
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Yo Yo',
+              description: 'Boss',
+              price: 'Seatle',
+              seller: '53',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            },
+            {
+              data: 'Tiger Nixon',
+              description: 'System Architect',
+              price: 'Boston',
+              seller: '61',
+            }
+            ]
+        }
+      }
+    }
+  }
 </script>
-
-
-
-
-<style lang="css"></style>
