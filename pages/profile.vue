57e3424baf1bd674a9809437fa7207ba5fb74458
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-25 12:08:15 -0400

    Update profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,58 +2,58 @@
 
 <div>
 
-<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 
 
 
-<div class="wrapper">
-    <div class="left">
-        <img src="../assets/user.png" alt="user" width="100">
-        <h4>User Name</h4>
-         <p>User Occupation</p>
-         <p>User Company</p>
-         <p>User Since</p>
-         
+  <div class="wrapper">
+      <div class="left">
+          <img src="../assets/user.png" alt="user" width="100">
+          <h4>User Name</h4>
+           <p>User Occupation</p>
+           <p>User Company</p>
+           <p>User Since</p>
+           
 
-    </div>
-    <div class="right">
-        <div class="info">
-            <h3>Information</h3>
-            <div class="info_data">
-                 <div class="data">
-                    <h4>Email</h4>
-                    <p>{{$store.state.user.email}}</p>
-                 </div>
-                 <div class="data">
-                   <h4>Phone</h4>
-                    <p>0001-213-998761</p>
-              </div>
-            </div>
-        </div>
-      
-      <div class="projects">
-            <h3>Projects</h3>
-            <div class="projects_data">
-                 <div class="data">
-                    <h4>Recent</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                 </div>
-                 <div class="data">
-                   <h4>Most Viewed</h4>
-                    <p>dolor sit amet.</p>
-              </div>
-            </div>
-        </div>
-      
-        <div class="social_media">
-            <ul>
-              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          </ul>
       </div>
-    </div>
-</div>
+      <div class="right">
+          <div class="info">
+              <h3>Information</h3>
+              <div class="info_data">
+                   <div class="data">
+                      <h4>Email</h4>
+                      <p>{{$store.state.user.email}}</p>
+                   </div>
+                   <div class="data">
+                     <h4>Phone</h4>
+                      <p>0001-213-998761</p>
+                </div>
+              </div>
+          </div>
+        
+        <div class="projects">
+              <h3>Projects</h3>
+              <div class="projects_data">
+                   <div class="data">
+                      <h4>Recent</h4>
+                      <p>Lorem ipsum dolor sit amet.</p>
+                   </div>
+                   <div class="data">
+                     <h4>Most Viewed</h4>
+                      <p>dolor sit amet.</p>
+                </div>
+              </div>
+          </div>
+        
+          <div class="social_media">
+              <ul>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+      </div>
+  </div>
 </div>
 
 
@@ -62,15 +62,16 @@
 
 <script>
 export default {
-	fetch({store, redirect}) {
-	if (
-	!store.state.user || 
-	(store.state.user && store.state.user.
-	isAuthenticated ===false)
-	) {
-	return redirect('/')
-	}
-	}
+
+  fetch({store, redirect}) {
+  if (
+  !store.state.user || 
+  (store.state.user && store.state.user.
+  isAuthenticated ===false)
+  ) {
+    return redirect('/')
+    }
+  }
 }
 </script>
 
