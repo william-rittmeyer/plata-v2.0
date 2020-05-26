4af3325f0c90ab1986826d09d033c47c7e6928f1
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-05-26 14:05:59 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,11 +35,17 @@
                   </nuxt-link>
 
                   <nuxt-link
-                    v-if="$store.state.user"
                     to="/database"
                     class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                   >
                   Database Access
+                  </nuxt-link>
+
+                  <nuxt-link
+                    to="/labeling"
+                    class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                  >
+                  Labeling
                   </nuxt-link>
                   
                   <nuxt-link
